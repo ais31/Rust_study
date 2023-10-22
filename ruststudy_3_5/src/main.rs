ecb11496875e86e50698ff7d8e026467d5a20543
ais31 <aiesvsaies@gmail.com>
2023-10-22 23:55:12 +0900

    fix

---
+++ ruststudy_3_5/src/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+fn main() {
+    let mut isloop : bool = true;
+    while isloop {
+        let mut temp = String::new();
+        println!(
+            "入力した温度を摂氏→華氏、華氏→摂氏に変換します。
+    まずは温度を入力してね(入力例：32)"
+        );
+
+        io::stdin()
+            .read_line(&mut temp)
+            .expect("Failed to read line"); // 行の読み込みに失敗しました
+
+        let temp: i32 = temp.trim().parse().expect("Please type a number!");
+
+        let mut cf = String::new();
+        println!("次は変換する温度を入力してね。摂氏にしたいならC,華氏にしたいならF。");
+
+        io::stdin().read_line(&mut cf).expect("Failed to read line"); // 行の読み込みに失敗しました
+        let _cf = match cf.as_str(){
+            "C" => {
+                let temp = convert_f_to_c(temp);
+                println!{"摂氏{}度だよ",temp};
+                isloop = false;
+            },
+            "F" => {
+                let temp = convert_c_to_f(temp);
+                println!{"華氏{}度だよ",temp};
+                isloop = false;
+            },
+            _ => {
+                println!("CかFを入力してね");
+            }
+        };
+    }
+}
+
+fn convert_f_to_c(x: i32) -> i32 {
+    x * 9 / 5 + 32
+}
+
+fn convert_c_to_f(x: i32) -> i32 {
+    (x - 32) * 5 / 9
+}
