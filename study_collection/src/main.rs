3fd71ca6cb00db17ac05850f16a2165e3b9e729a
ais31 <aiesvsaies@gmail.com>
2023-11-09 01:17:39 +0900

    WIP

--- study_collection/src/main.rs
+++ study_collection/src/main.rs
@@ -1,15 +1,32 @@
-struct {
+struct Matome1 {
     mean :i64,
     median :i64,
     mode :i64,
 }
 
-fn main() {
-    
-    let v: Vec<i32> = Vec::new();
-    let v = vec![1,2,3,4];
-
-
+impl Matome1 {
+    fn new() -> Matome1 {
+        Matome1 {mean:0,median:0,mode:0}
+    }
 }
 
-fn no1(vec:Vec<i32>) ->
+fn main() {
+    
+    // let v: Vec<i32> = Vec::new();
+    let v = vec![1,2,3,4];
+
+    let matome1 = matome_calc_1(v);
+
+    //println!("計算結果は{:?}", matome1);
+}
+
+fn matome_calc_1(vec:Vec<i32>) -> Matome1{
+    let mut matome1 = Matome1::new();
+    let mut sum = 0;
+    for i in &vec {
+        sum += i;
+    }
+    matome1.mean = sum / vec.len();
+    println!(matome1.mean );
+
+}
