a363a8485cb90b91f1d01df43b58ead655c1051b
ais31 <aiesvsaies@gmail.com>
2023-11-11 01:29:44 +0900

    WIP

--- study_collection/src/main.rs
+++ study_collection/src/main.rs
@@ -14,21 +14,38 @@ impl Matome1 {
 fn main() {
     
     // let v: Vec<i32> = Vec::new();
-    let v = vec![1,2,3,4];
+    let v: Vec<i32> = vec![1,8,3,6,5];
 
-    let matome1 = matome_calc_1(v);
+    let matome1: Matome1 = matome_calc_1(v);
 
     println!("計算結果は{:?}", matome1);
 }
 
-fn matome_calc_1(vec:Vec<i32>) -> Matome1{
+fn matome_calc_1(mut vec:Vec<i32>) -> Matome1{
     let mut matome1 = Matome1::new();
-    let mut sum = 0.0;
+    let mut sum: f32 = 0.0;
+    //平均値を求める
     for i in &vec {
         sum += *i as f32;
     }
     matome1.mean = sum / vec.len() as f32;
     println!("{}",matome1.mean.to_string() );
+
+    //中央値を求める
+    vec.sort();
+    let mid :usize = vec.len() / 2;
+    if vec.len() % 2 == 0 {
+        matome1.median = (vec[mid -1] as f32 + vec[mid] as f32 )/ 2.0;
+    } else {
+        matome1.median = vec[mid] as f32;
+    }
+    println!("{}",matome1.median.to_string() );
+
+    //最頻値を求める
+
+
+
+    
 
     matome1
 
